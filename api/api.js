7f0c6ef4cd8ebef336630d26a88bc3725d7615c1
Andrea Zecca <andrea.zecca3@studio.unibo.it>
2021-11-09 19:09:36 +0100

    Fixed user timeline route

--- api/api.js
+++ api/api.js
@@ -66,7 +66,6 @@ router.get('/user/:username', async (req, res) => {
 router.get('/statuses/user_timeline', async (req, res) => {
   const query = req.query || {}
   try{
-    console.log(process.env)
     const response = await twitter.v1.get('statuses/user_timeline.json', { query })
     res.status(200).json(response)
   } catch (e) {
